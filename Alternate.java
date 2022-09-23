b322720671b01bb45b587c2aaeca9112f84fce25
Adnan2107 <113004812+Adnan2107@users.noreply.github.com>
2022-09-23 10:13:28 +0530

    Add files via upload

--- Alternate.java
+++ Alternate.java
@@ -2,71 +2,36 @@
 public class Alternate {
 
 
-class ABC {
+public static int  printAlternetNo(int[]arr1) 
+	{
+		 
+	        int total=0;
+	        
+	        System.out.println("Alternating number are" );
+	        for (int i = 0; i < arr1.length; i = i+2) 
+	        {  
+	
+				System.out.println(arr1[i]);
+				
+				total +=arr1[i];
+	        }  
+			
+	        return total;
+	      
+	}
+	
+	
+	public static void main(String[] args) 
+	{     
+	       int [] arr1 ={1, 2, 3, 4, 5,6,7,8,9,1};  
 
-  
-
-    static void alternateMerge(int arr1[], int arr2[], 
-
-                               int n1, int n2, int arr3[])
-
-    {
-
-        int i = 0, j = 0, k = 0;
-
-     
-        while (i < n1 && j < n2)
-
-        {
-
-            arr3[k++] = arr1[i++];
-
-            arr3[k++] = arr2[j++];
-
-        }
-
-     
-
-        while (i < n1)
-
-            arr3[k++] = arr1[i++];
-
-     
-
-        while (j < n2)
-
-            arr3[k++] = arr2[j++];
-
-    }
-
-   
-    public static void main(String args[])
-
-    {
-
-        int arr1[] = {1, 3, 5, 7, 9, 11};
-
-        int n1 = arr1.length;
-
-     
-
-        int arr2[] = {2, 4, 6, 8};
-
-        int n2 = arr2.length;
-
-     
-
-        int arr3[] = new int[n1+n2];
-
-        alternateMerge(arr1, arr2, n1, n2, arr3);
-
-	        System.out.println("Array after merging");
-
-	        for (int i = 0; i < n1 + n2; i++)
-
-	            System.out.print( arr3[i] +" ");
-
-	    }
+	       int total=printAlternetNo(arr1);
+		System.out.print("Addition Of Alternating number :" +total);
 	}
 
-}
+
+
+	    }
+	
+
+
