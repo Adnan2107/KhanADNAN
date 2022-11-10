0fb9f431a9ea82fcd24a2d69c9a2732b2fa8ee90
Adnan2107 <113004812+Adnan2107@users.noreply.github.com>
2022-11-10 11:42:20 +0530

    Add files via upload
    first commit

---
+++ ForDemo/src/ForDemo.java
@@ -0,0 +1,17 @@
+
+public class ForDemo {
+
+	public static void main(String[] args) {
+		   
+		   for(int i=2;i<=20;i=i+2)
+		   {
+			 System.out.println(i);
+		   }
+		     
+		   System.out.println("End of loop");
+	      }
+	
+	
+	}
+
+
