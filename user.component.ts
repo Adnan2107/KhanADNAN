6e04ae2ac0c822ae454f4f1bac6bf84682d609aa
Adnan2107 <113004812+Adnan2107@users.noreply.github.com>
2023-01-07 12:30:57 +0530

    Add files via upload

---
+++ user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  name = new FormControl();
+  pass = new FormControl();
+
+  login(){
+    alert("logged in");
+  }
+
+}
